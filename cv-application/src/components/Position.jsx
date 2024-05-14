fc2c777aad4b854cdd905aeaac7f09505b3500b8
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-14 11:43:47 -0400

    style: increase margin of position component

--- cv-application/src/components/Position.jsx
+++ cv-application/src/components/Position.jsx
@@ -1,7 +1,7 @@
 function Position () {
     return (
         <>
-            <div className="text-center">
+            <div className="mb-10 text-center">
                 <h1 className="text-4xl font-bold">Bartender</h1>
                 <p>Welcome to the Black Lagoon Cafe!</p>
             </div>
