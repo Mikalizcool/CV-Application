4b7f227eb78fbfa00080df7aa9195afeccda94cb
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-14 12:02:38 -0400

    feat: improve introductory message

--- cv-application/src/components/Position.jsx
+++ cv-application/src/components/Position.jsx
@@ -3,7 +3,9 @@ function Position () {
         <>
             <div className="mb-10 text-center">
                 <h1 className="text-4xl font-bold">Bartender</h1>
-                <p>Welcome to the Black Lagoon Cafe!</p>
+                <p>Thank you for your interest in employment with the Black Lagoon Cafe! <br />
+                As the <b>Bartender</b>, you will be the most important person in our establishment! <br />
+                Please fill out the form below and one of our managers will get back to you ASAP!</p>
             </div>
         </>
     )
